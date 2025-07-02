9bcabdbed5a5ed126213bc43319b78973c0f8102
ppaipi <137942755+ppaipi@users.noreply.github.com>
2025-07-01 22:33:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+
+const CONFIG = {
+    GOOGLE_MAPS_API_KEY: 'AIzaSyBVozj7g2eddN7aXyE8VeAaNKdPMtYdsQ4',
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzffgdmmKb5hYfyDxew1tzVb_DA7eQkW5c6pH6XO9nBklvnKNUb2jMulmF2Uj437XWV/exec',
+    LOCAL_ADDRESS: 'Ibera 3852, Coghlan, CABA, Argentina'
+  };
+  
