8618a4025d0fe9525872edbc19fc3c617ebc9eb7
ppaipi <137942755+ppaipi@users.noreply.github.com>
2025-07-23 22:53:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ function createVerMasCard(categoria) {
   const icon = document.createElement('div');
   icon.className = 'ver-mas-icon';
   icon.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="#1E88E5">
+    <svg xmlns="http://www.w3.org/2000/svg" width="130" height="130" viewBox="0 0 24 24" fill="#1E88E5">
       <rect x="10" y="4" width="4" height="16" rx="1"/>
       <rect x="4" y="10" width="16" height="4" rx="1"/>
     </svg>
