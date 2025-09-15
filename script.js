1474818ba316d1537b01bcf5f76bead74457c2f0
Felipe Chapur <felipechapur@gmail.com>
2025-09-15 19:20:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -194,8 +194,8 @@ function cargarDiasEntrega() {
 
   const diasValidos = [1, 4]; // Lunes (1), Jueves (4)
   const horasCorte = { 
-    1: 13, // Lunes → límite 14hs
-    4: 13  // Jueves → límite 14hs
+    1: 14, // Lunes → límite 14hs
+    4: 14  // Jueves → límite 14hs
   };
 
   const opciones = [];
