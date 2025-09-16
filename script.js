55e62ef086ac3305751595533fac2fc4be41045e
Felipe Chapur <felipechapur@gmail.com>
2025-09-16 17:54:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1172,7 +1172,7 @@ window.onload = () => {
 
   const searchInput = document.getElementById('search-input');
   const clickHeader = document.getElementById('click_header');
-  const botonContacto = document.gebtn-contacto-tienda("btn-contacto-tienda");
+  const botonContacto = document.getElementsByClassName("btn-contacto-tienda");
   if (searchInput) {
     searchInput.addEventListener('input', () => searchProduct());
   }
